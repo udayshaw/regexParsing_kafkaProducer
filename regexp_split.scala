07afe69381445dc9fd5f862dc5b4f38e9b173018
Uday Shaw <46369288+udayshaw@users.noreply.github.com>
2020-09-15 10:16:43 +0530

    Update regexp_split.scala

--- regexp_split.scala
+++ regexp_split.scala
@@ -1,5 +1,1 @@
-val regex="""\[(\S*\:\S*|\d*\.\d*\.\d*\.\d*)\] - (\d*\.\d*\.\d*\.\d*) - \[(\d*?\S*?\S*\:\d*\:\d*\:\d*) \+\d*\] \"(\S*) (\S*) (\S*)\" (\d*) (\d*) \"\-\" \"(\S*)\"\"(\d*\.\d*\.\d*\.\d*\:\d*)\" \"(\S*)\" \"(\S*)\" .*$"""
-
-import org.apache.spark.sql.functions.{col, udf}
-val regexp_split=udf((value : String, regex: String) => { regex.r.unapplySeq(value)})
-df.select(regexp_split(col("value"),lit(regex)).as("data")).show(false)
+no code here
